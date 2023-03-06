4c45c1a3d14155beb3733bf3e0e201bebb134ecf
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2023-03-06 13:16:33 -0700

    Solved issues with data transfer

--- translator/src/components/translation.jsx
+++ translator/src/components/translation.jsx
@@ -1,29 +1,66 @@
 import React from 'react'
 
-const translation = () => {
-    const translatedArray = []
-   const translateFrom = Array.from (val.toLowerCase())
-   console.log(translateFrom)
+const translation = ({message}) => {
+    
+
+
+   const translatedArray = []
+   const translateFrom = Array.from (message.toLowerCase())
    
    for (let i = 0; i < translateFrom.length; i++) {
     switch (translateFrom[i]) {
-      case 'a' :
-        console.log('it worked! ')
+    
+       case 'a' :
+            translatedArray.push('ᚨ')
+        break;
+
+        case 'f' :
+          translatedArray.push("ᚠ")
+          break;
+        case 'a' :
+        translatedArray.push('ᚨ')
 
         break;
       case 'f' :
         console.log('this should be first') 
-        translatedArray.push("yay")
+        translatedArray.push("ᚠ")
+        case 'a' :
+        translatedArray.push('ᚨ')
+
+        break;
+      case 'f' :
+        console.log('this should be first') 
+        translatedArray.push("ᚠ")
+        case 'a' :
+        translatedArray.push('ᚨ')
+
+        break;
+      case 'f' :
+        console.log('this should be first') 
+        translatedArray.push("ᚠ")
+        case 'a' :
+        translatedArray.push('ᚨ')
+
+        break;
+      case 'f' :
+        console.log('this should be first') 
+        translatedArray.push("ᚠ")
     }
     console.log(translatedArray)
    }
-    return translatedArray
-  }
+
+    
+
+
   return (
     <div>
-      
+     <h2>this is where the translation will be:</h2>
+     <br/>
+     <div>
+        <p>  {translatedArray} </p>
+     </div>
     </div>
   )
-}
+  } 
 
 export default translation
