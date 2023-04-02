9bbd9ca3466532fa6db448e6eb2358b1043ca29c
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2023-04-02 15:13:04 -0700

    added an acticle folder and function

---
+++ translator/src/Articles/Articles.jsx
@@ -0,0 +1,4 @@
+export const Articles = [
+    {"article1" : "A rune is a letter in a set of related alphabets known as runic alphabets native to the Germanic peoples. Runes were used to write various Germanic languages (with some exceptions) before they adopted the Latin alphabet, and for specialised purposes thereafter."},
+    
+];
--- translator/src/components/Discovery/Discovery.jsx
+++ translator/src/components/Discovery/Discovery.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import "./Discovery.css"
 import ship from '../../assets/vikingart.jpg'
+import  {Articles}  from '../../Articles/Articles'
+
+// return article data from an article folder, easier to edit and cleaner code.  
+const data = Articles.map(element => {
+    return element.article1
+})
 
 const Discovery = () => {
   return (
     <div className='container-sm'>
         <div className='row4'>
-        <h1>Why Runes?</h1>
+        <h1>What are Runes?</h1>
         </div>
         <div className='row5'>
             <div className='col-8'>
-                hailey is fluent in satanic rune
+            {data}
             </div>
             <div className='col-4'>
                 <img src={ship} alt="ship art" className='img-fluid ship' />
