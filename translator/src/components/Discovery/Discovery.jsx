5fd22772a02c012b18603860917ff15bed44cbd8
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2023-04-04 10:41:20 -0700

    dried out code

--- translator/src/components/Discovery/Discovery.jsx
+++ translator/src/components/Discovery/Discovery.jsx
@@ -3,10 +3,16 @@ import "./Discovery.css"
 import ship from '../../assets/vikingart.jpg'
 import  {Articles}  from '../../Articles/Articles'
 
-// return article data from an article folder, easier to edit and cleaner code.  
-const data = Articles.map(element => {
-    return element.article1
-})
+// return article data from an article folder, easier to edit and cleaner code. 
+
+// put articles in array for no reason, updated it t
+// const data = Articles.map(element => {
+//     return element.article1
+// })
+
+const article1 = Articles.article1
+
+console.log(article1)
 
 const Discovery = () => {
   return (
@@ -16,11 +22,11 @@ const Discovery = () => {
         </div>
         <div className='row5'>
             <div className='col-8 fade-in'>
-            {data}
+            {article1}
             <cite> <a href='https://en.wikipedia.org/wiki/Runes#:~:text=A%20rune%20is%20a%20letter,and%20for%20specialised%20purposes%20thereafter.'>-Runes-Wikipedia</a></cite>
             </div>
             <div className='col-4'>
-                <img src={ship} alt="ship art" className='img-fluid ship' />
+                <img src={ship} alt="ship art" className='img-fluid ship fade-in' />
             </div>
         </div>
         <div className='row5'>
