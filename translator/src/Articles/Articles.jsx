febe80817b0bbae034436c8775cc9364e6c2c56c
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2023-04-04 12:26:04 -0700

    Added 2nd article, needs more styling

--- translator/src/Articles/Articles.jsx
+++ translator/src/Articles/Articles.jsx
@@ -1,4 +1,5 @@
-export const Articles = [
-    {"article1" : "A rune is a letter in a set of related alphabets known as runic alphabets native to the Germanic peoples. Runes were used to write various Germanic languages (with some exceptions) before they adopted the Latin alphabet, and for specialised purposes thereafter."},
+export const Articles = 
+    {article1 : "A rune is a letter in a set of related alphabets known as runic alphabets native to the Germanic peoples. Runes were used to write various Germanic languages (with some exceptions) before they adopted the Latin alphabet, and for specialised purposes thereafter.",
+    article2 : "My son and I were off on another adventure searching for a well hidden waterfall by a small town in central Arizona.  While we were tyring to find an indictor of which direction the waterfall might be we came across a few permitive shelters. One of the small shelters had strange markings on the front cross beam.  I had no idea what they they could be, so I took a picture to allow myself to investigate further. "}
     
-];
+;
